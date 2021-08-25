037a9039c3598678424baff6d632dc906d6ece85
Bruno Luna <luna.brsilva@gmail.com>
2021-08-25 20:07:14 -0300

    [criando GET, PUT, POST e DELETE do Usuario]

--- Spring_Boot/blogPessoal/blogPessoal/src/main/java/com/generation/blogPessoal/repositorios/UsuarioRepositorio.java
+++ Spring_Boot/blogPessoal/blogPessoal/src/main/java/com/generation/blogPessoal/repositorios/UsuarioRepositorio.java
@@ -1,5 +1,7 @@
 package com.generation.blogPessoal.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,13 @@ import com.generation.blogPessoal.modelos.Usuario;
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
 
+	/**
+	 * Metodo utilizado para pesquisar coluna nome ContainingIgnoreCase
+	 * 
+	 * @param nome do tipo String
+	 * @return List de Usuario
+	 * 
+	 * @author Turma29
+	 */
+	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
