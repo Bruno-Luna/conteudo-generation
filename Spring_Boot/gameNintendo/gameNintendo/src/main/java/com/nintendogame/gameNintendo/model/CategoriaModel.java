d0986e8935690f19a20b853d4d3e1934efbda202
Bruno Luna <luna.brsilva@gmail.com>
2021-09-11 10:35:24 -0300

    [Inserção das dependencias Security, retirando a Loombok das classes CategoriaModel e ProdutoModel]

--- Spring_Boot/gameNintendo/gameNintendo/src/main/java/com/nintendogame/gameNintendo/model/CategoriaModel.java
+++ Spring_Boot/gameNintendo/gameNintendo/src/main/java/com/nintendogame/gameNintendo/model/CategoriaModel.java
@@ -1,16 +1,22 @@
 package com.nintendogame.gameNintendo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 @Entity
 public class CategoriaModel {
 
@@ -22,4 +28,41 @@ public class CategoriaModel {
 	
 	@NotBlank
 	public String descricao;
-}
+	
+	@OneToMany(mappedBy = "origin", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"origin"})
+	private List<ProdutoModel> meusProdutos = new ArrayList<>();
+
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<ProdutoModel> getMeusProdutos() {
+		return meusProdutos;
+	}
+
+	public void setMeusProdutos(List<ProdutoModel> meusProdutos) {
+		this.meusProdutos = meusProdutos;
+	}
+	
+	}
--- Spring_Boot/gameNintendo/gameNintendo/src/main/java/com/nintendogame/gameNintendo/model/ProdutoModel.java
+++ Spring_Boot/gameNintendo/gameNintendo/src/main/java/com/nintendogame/gameNintendo/model/ProdutoModel.java
@@ -4,14 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 @Entity
+
 public class ProdutoModel {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +29,51 @@ public class ProdutoModel {
 	
 	@NotBlank
 	private String fabricante;
+	
+	@ManyToOne
+	@JoinColumn(name = "origin_id")
+	@JsonIgnoreProperties({"meusProdutos"})
+	private CategoriaModel origin;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public CategoriaModel getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(CategoriaModel origin) {
+		this.origin = origin;
+	}
+	
+	
 }
