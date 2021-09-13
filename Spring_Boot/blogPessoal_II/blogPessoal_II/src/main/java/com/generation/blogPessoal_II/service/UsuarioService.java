49bd4a4b8a7dc59a52193c192660f40e49f215b0
Bruno Luna <luna.brsilva@gmail.com>
2021-09-13 14:39:19 -0300

    [Criação do Package Service e sua classe UsuarioService]

---
+++ Spring_Boot/blogPessoal_II/blogPessoal_II/src/main/java/com/generation/blogPessoal_II/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.generation.blogPessoal_II.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.generation.blogPessoal_II.model.UsuarioModel;
+import com.generation.blogPessoal_II.model.utilities.UsuarioLogin;
+import com.generation.blogPessoal_II.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Optional<Object> cadastrarUsuario(UsuarioModel novoUsuario){
+		return repository.findByEmail(novoUsuario.getEmail()).map(usuarioExistente ->{
+			return Optional.empty();
+		}).orElseGet(() ->{
+			BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+			String result = enconder.encode(novoUsuario.getSenha());
+			novoUsuario.setSenha(result);
+			return Optional.ofNullable(repository.save(novoUsuario));
+		});
+	}
+	public Optional<?> autenticador (UsuarioLogin usuarioAutenticar){
+		return repository.findByEmail(usuarioAutenticar.getEmail()).map(usuarioExistente ->{
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();	
+		
+			if(enconder.matches(usuarioAutenticar.getSenha(), usuarioExistente.getSenha())) {
+				String estruturaBasic = usuarioAutenticar.getEmail() + ":" + usuarioAutenticar.getSenha();
+				byte[] autorizacaoBase64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII")));
+				String autorizacaoHeader = "Basic " + new String (autorizacaoBase64);
+				
+				usuarioAutenticar.setToken(autorizacaoHeader);
+				usuarioAutenticar.setId(usuarioExistente.getIdUsuario());
+				usuarioAutenticar.setNome(usuarioExistente.getNome());
+				usuarioAutenticar.setSenha(usuarioExistente.getSenha());
+				return Optional.ofNullable(usuarioAutenticar);
+
+			} else {
+				return Optional.empty(); // Senha errada
+			}
+		}).orElseGet(() -> {
+			return Optional.empty(); // Email não existe	
+		});
+	}
+	
+}
