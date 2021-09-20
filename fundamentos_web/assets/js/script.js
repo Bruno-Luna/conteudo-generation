690c641f213993ebd6841cd5f32fcbe9e545da1b
Bruno Luna <luna.brsilva@gmail.com>
2021-09-20 19:46:00 -0300

    Criação das funções e validações dos inputs

---
+++ fundamentos_web/assets/js/script.js
@@ -0,0 +1,97 @@
+let nome = document.getElementById('nome');
+let email = document.querySelector('#email');
+let idade = document.querySelector('input#idade');
+let assunto = document.querySelector('#assunto');
+let mapa = document.querySelector('#mapa');
+
+let nomeOk = false;
+let emailOk = false;
+let idadeOk = false;
+let assuntoOk = false;
+
+nome.style.width = '100%'
+email.style.width = '100%'
+idade.style.width = '100%'
+assunto.style.width = '100%'
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3 || nome.value == Number){
+        txtNome.innerHTML = 'Nome invalido'
+        txtNome.style.color = '#FF0000'
+    }else{
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = '#008000'
+        nomeOk = true
+    }
+}
+
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@')== -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'Email inválido'
+        txtEmail.style.color = '#FF0000'
+    }else{
+        txtEmail.innerHTML = 'Email válido'
+        txtEmail.style.color = '#008000'
+        emailOk = true
+    }
+}
+
+function validaIdade(){
+    let txtIdade = document.getElementById('txtIdade');
+     if(idade.value <= 0 || idade.value >= 120){
+        txtIdade.innerHTML = 'Idade inválida'
+        txtIdade.style.color = '#FF0000'
+    }else{
+        txtIdade.innerHTML = 'Idade válida'
+        txtIdade.style.color = '#008000'
+        idadeOk = true
+    }
+}
+
+function validaRadio() {
+    if (document.formulario.assunto[0].checked == false 
+      && document.formulario.assunto[1].checked == false 
+      && document.formulario.assunto[2].checked == false) {
+      alert('Por favor, selecione o assunto.');
+      return false;
+    }else{
+        return true;
+
+    }
+    
+  }
+
+function validaAssunto(){
+   let txtAssunto = document.querySelector('#txtAssunto');
+
+   if (assunto.value.length >= 100){
+       txtAssunto.innerHTML = 'O texto é muito grande, por favor reduza-o.'
+       txtAssunto.style.color = '#FF0000'
+       txtAssunto.style.display = 'block'
+   }else
+    txtAssunto.style.display = 'none'
+    assuntoOk = true
+}
+
+function enviar(){
+    if(nomeOk == true && emailOk == true && idadeOk && assuntoOk == true){
+        alert('Formulário enviado com sucesso!')
+    }else{
+        alert('Por favor, preencha todos os dados corretamente.')
+    }
+}
+
+function ampliarMapa(){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '450px'
+    mapa.style.height = '300px'
+}
+
